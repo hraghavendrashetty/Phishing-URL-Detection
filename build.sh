347e8fdd7c4f22b931b1f18614358735ee16ef6e
hraghavendrashetty <hraghavendrashetty13@gmail.com>
2025-10-16 23:54:45 +0530

    Add Render config and update build process

--- build.sh
+++ build.sh
@@ -2,9 +2,12 @@
 # exit on error
 set -o errexit
 
-# First update pip and install build essentials
-python -m pip install --upgrade pip
-pip install setuptools==57.0.0 wheel==0.37.0
+# Create and activate a new virtual environment
+python -m venv venv
+source venv/bin/activate
 
-# Install the requirements
-pip install --no-cache-dir -r requirements.txt
+# Install build dependencies
+pip install --upgrade pip setuptools==57.0.0 wheel==0.37.0
+
+# Install requirements using only binary packages
+pip install --only-binary=:all: -r requirements.txt
